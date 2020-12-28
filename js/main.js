71d471ff5e656913701bce1cb9ef0fe26b92ac5a
Anjan <anjan.bhattrai@gmail.com>
2020-12-28 17:37:44 -0600

    final changes

--- js/main.js
+++ js/main.js
@@ -10,17 +10,6 @@
         $("#preloder").delay(200).fadeOut("slow");
     });
 
-    //Search Switch
-    $('.search-switch').on('click', function () {
-        $('.search-model').fadeIn(400);
-    });
-
-    $('.search-close-switch').on('click', function () {
-        $('.search-model').fadeOut(400, function () {
-            $('#search-input').val('');
-        });
-    });
-
     /*------------------
         Navigation
     --------------------*/
